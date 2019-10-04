0de7497ea38ab4985e3eaf5c5bef226b546f9669
JoseLuis1308 <jose.l.l.p.13@gmail.com>
2019-10-04 12:36:34 +0200

    tareas y pruebas

--- Ejercicios/EjerciciosUT2/EjerciciosControl/JS/TablaMultiplicar.js
+++ Ejercicios/EjerciciosUT2/EjerciciosControl/JS/TablaMultiplicar.js
@@ -6,7 +6,6 @@ if(respuesta){
     for(let i=1;i<=10;i++){
         for(let j=1;j<=10;j++){
             cadena+=i+" * "+j+" = "+(i*j)+"<br/>";
-            
         }
     }
     document.getElementById("tablas").innerHTML=cadena;
